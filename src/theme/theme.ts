7bb8b27e18b632d1749ee2d42be6ac2fcb1b9126
Marcelo Henrique Ribeiro <mghenr@gmail.com>
2023-09-12 14:01:09 -0300

    feat color palette

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,12 +3,14 @@ import { createTheme } from '@mui/material';
 const theme = {
   primary: {
     main: '#428aea',
+    light: '#8cb8f2',
+    dark: '#155ebf',
   },
   secondary: {
     main: '#fed14f',
+    light: '#fede80',
+    dark: '#d39d01',
   },
-  contrastThreshold: 3,
-  tonalOffset: 0.2,
 };
 
 export const muiTheme = createTheme({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          main: '#428aea',
+          light: '#8cb8f2',
+          dark: '#155ebf',
+          'extra-light': '#d9e7fb',
+          'extra-dark': '#0d3873',
+        },
+        secondary: {
+          main: '#fed14f',
+          light: '#fede80',
+          dark: '#d39d01',
+          'extra-light': '#fff4d5',
+          'extra-dark': '#7f5e01',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
